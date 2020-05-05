c063be3fbcf16a0be594870cf85cbc044900810c
Balaji Saravanan <balaji.saravanan@zohocorp.com>
2020-05-06 02:07:38 +0530

    pushed collections to mongo db created an api

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts)
+
+
+app.get('/', (req, res) => res.send('Hello'));
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => console.log(`App started at ${port}`));
---
+++ server/routes/api/posts.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get the collections via query params of id or stock symbol
+router.get('/', async(req, res) => {
+  const posts = await loadPostsCollection();
+  if (req.query.symbol) {
+    res.send(await posts.find({ stock_symbol: req.query.symbol }).toArray());
+  } else if (req.query.id) {
+    let id = Number(req.query.id);
+    res.send(await posts.find({ _id: id }).toArray());
+  } else {
+    res.send(await posts.find({}).toArray());
+  }
+});
+
+// Get the collections via normal params of id
+router.get('/:id', async(req, res) => {
+  const posts = await loadPostsCollection();
+  let id = Number(req.params.id);
+  res.send(await posts.find({ _id: id }).toArray());
+});
+
+
+// Add the posts
+const pushCollection = async() => {
+    // const jsonData = require('./../../stock_mock');
+    const posts = await loadPostsCollection();
+    jsonData.forEach((data, index) => {
+      data._id = index;
+      posts.insertOne(data);
+    });
+  }
+  // pushCollection();
+
+
+
+// Delete collection
+const deleteCollection = async() => {
+    const posts = await loadPostsCollection();
+    posts.deleteMany();
+  }
+  // deleteCollection();
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect('mongodb+srv://root:root@cluster0-8nvky.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  return client.db('test').collection('stock-data');
+}
+
+module.exports = router;
