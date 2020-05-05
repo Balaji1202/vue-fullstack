adb69887f8ba18b63726a93d4b36e67fab3d1fdb
Balaji Saravanan <balajilitsv@gmail.com>
2020-05-06 02:32:20 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = require('./routes/api/posts');
+const posts = require('./routes/api/stock-mock');
 
 app.use('/api/posts', posts)
 
